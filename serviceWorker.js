7a6127aad9891a7ea315c4e9c0e2118c6145f63e
Danny J <dj3636@ou.ac.uk>
2021-03-15 21:24:02 +0000

    changes

--- serviceWorker.js
+++ serviceWorker.js
@@ -7,6 +7,7 @@ const assets = [
   "/css/style.css",
   "/css/styles.css",
   "/js/app.js",
+<<<<<<< HEAD
   "/images/assets",
   "/images/icons/icon-72x72.png",
   "/images/icons/icon-96x96.png",
@@ -16,6 +17,16 @@ const assets = [
   "/images/icons/icon-192x192.png",
   "/images/icons/icon-384x384.png",
   "/images/icons/icon-512x512.png",
+=======
+  "/images/icons/icon-72x48.png",
+  "/images/icons/icon-96x64.png",
+  "/images/icons/icon-128x85.png",
+  "/images/icons/icon-144x95.png",
+  "/images/icons/icon-152x101.png",
+  "/images/icons/icon-192x127.png",
+  "/images/icons/icon-384x255.png",
+  "/images/icons/icon-512x339.png"
+>>>>>>> parent of 35cb045 (updated images sizes for web app)
 ]
 
 self.addEventListener("install", installEvent => {
