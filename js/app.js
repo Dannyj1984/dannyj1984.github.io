32e5ab4e0a12cb15fbf87dd0bf1c3c07a7d41ec9
Danny J <dj3636@ou.ac.uk>
2021-03-16 21:37:00 +0000

    add modal info popup on load

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
-if ("serviceWorker" in navigator) {
+if ("serviceWorker" in navigator && PushManager in window) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
         .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
+        .then(res => console.log("service worker registered and push allowed"))
         .catch(err => console.log("service worker not registered", err))
     })
+  } else {
+    console.warn('Push messaging is not supported');
+    pushButton.textContent = 'Push Not Supported';
   }
