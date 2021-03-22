cb34c1f8c667f05f3b5f3a76d8e39a7af50ff06e
Danny J <dj3636@ou.ac.uk>
2021-03-22 19:43:18 +0000

    added sw registration.update()

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ if ("serviceWorker" in navigator) {
       navigator.serviceWorker
         .register("/serviceWorker.js")
         .then(res => console.log("service worker registered"))
+        button.onclick = function() {
+          registration.update();
+        }
         .catch(err => console.log("service worker not registered", err))
+        
+        
     })
   }
